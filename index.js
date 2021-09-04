0c4bf31379fb0ec3d4854b0b04093ad67e5d2ef2
Mozert Dutra <84636609+mozert1988@users.noreply.github.com>
2021-09-04 21:04:15 +0000

    Arrumando erro de Repetir jogador nas rodadas

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
 let quantidadeRodadas = 0;
 let numeroJogadores = 0;
 let listaJogadores = [];
-let jogarNovamente = 0;
 
 console.log("---------------------Jogo de Dados--------------------------\n")
 quantidadeRodadas = prompt("Olá, vamos começar! Quantas rodadas você quer jogar? ");
 
 numeroJogadores = prompt("Quantos jogadores vão participar?");
 
+// Criação de objetos dos jogadores
 for(var contadorJogador = 1; contadorJogador <= numeroJogadores; contadorJogador++){
   var objetoJogadores = new Object ();
   objetoJogadores.nome = prompt("Digite o nome do jogador numero " + contadorJogador + ":"); 
   objetoJogadores.numeroDado = 0;
   objetoJogadores.rodadasGanhas = 0; 
-
+  
+  // Adicionando cada objeto na lista
   listaJogadores.push(objetoJogadores);
 }
 
+//Iterando a quandidade de jogadas definidas no inicio
 for (var contador = 1; contador <= quantidadeRodadas; contador++){
   console.log('Rodada ' + contador);
 
   for (var contadorJogador = 0; contadorJogador < listaJogadores.length; contadorJogador++){
     prompt("O jogador " + listaJogadores[contadorJogador].nome + " deve apertar enter para jogar o dado");
 
-    var resuldadoDado = Math.floor(Math.random() * (6 - 1)) + 1;
-
-    listaJogadores[contadorJogador].numeroDado = resuldadoDado;
-    console.log(listaJogadores[contadorJogador].nome + " tirou " + resuldadoDado);
+    listaJogadores[contadorJogador].numeroDado =  Math.floor(Math.random() * (6 - 1)) + 1;
+    
+    console.log(listaJogadores[contadorJogador].nome + " tirou " + listaJogadores[contadorJogador].numeroDado);
   }
   
+  // Ordenandoa lista de jogadores por quem tirou o maior numero no dado
   listaJogadores.sort(function (x,y){
     return x.numeroDado - y.numeroDado
   }).reverse();
@@ -37,8 +39,10 @@ for (var contador = 1; contador <= quantidadeRodadas; contador++){
   console.log(listaJogadores[0].nome + " ganhou a rodada " + contador);
 }
 
+//Ordenando a lista de jogadores por quem ganhou mais rodadas. 
 listaJogadores.sort(function (x,y){
   return x.rodadasGanhas - y.rodadasGanhas
 }).reverse();
 
+//Apresentando o resultado final de quem foi o ganhador. 
 console.log(listaJogadores[0].nome + " ganhou " + listaJogadores[0].rodadasGanhas + " rodadas e foi o grande campeão");
